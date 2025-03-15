670c66a38d0d5b6bdb0a524fe310ffb864cb35d3
Noras2001 <nora.serdyukova@gmail.com>
2025-03-15 16:01:55 -0300

    new branch for filtering experiments

--- student_shop/store/models.py
+++ student_shop/store/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db import models
+from django.utils import timezone
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -17,6 +19,22 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.product.name} ({self.quantity})"
+
+from django.db import models
+from django.utils import timezone
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    category = models.CharField(max_length=100, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    orders_count = models.PositiveIntegerField(default=0)  # для популярности
+
+    def __str__(self):
+        return self.name
+
     
 
 
